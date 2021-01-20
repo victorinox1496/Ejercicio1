dda4b63b20887cae46bf5d657a2f3a55593391da
victorinox1496 <victorinox1496@gmail.com>
2021-01-20 11:32:45 -0400

    commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+
+    <?php
+      function mult($x,$y){
+          $res=$x/(1/$y);
+         echo $res;
+         //var_dump($res);
+      }
+mult(20,10);
+?>
+</body>
+</html>
